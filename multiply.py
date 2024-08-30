633b87bf4012949e26f6238da63acd384760cab5
sailendraprasath <sailendraprasath05@gmail.com>
2024-08-30 19:41:18 +0530

    new file is multiply.py

---
+++ multiply.py
@@ -0,0 +1,4 @@
+"""Write a program to print a multiplication table. If user gives 5 as input, print multiplication table of 5."""
+val = 5
+for i in range(1, 11):
+    print(f"{val} x {i} = {val * i}")
